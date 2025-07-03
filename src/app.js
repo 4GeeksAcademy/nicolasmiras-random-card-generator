495de4a7d0aea1dbe2c4f404edbc6015bdea91be
Nico Miras <73142724+nicolasmiras@users.noreply.github.com>
2025-07-03 00:44:36 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,26 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //escribe tu codigo aca
-  let cardNumbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+  let cardNumbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
   let cardType = [
-  { type: "♠", color: "black" },
-  { type: "♥", color: "red" },
-  { type: "♦", color: "red" },
-  { type: "♣", color: "black" }
-];
+    { type: "♠", color: "black" },
+    { type: "♥", color: "red" },
+    { type: "♦", color: "red" },
+    { type: "♣", color: "black" }
+  ];
 
+  let randomType = cardType[Math.floor(Math.random() *4)];
 
-  let randomNumber = cardNumbers[Math.floor(Math.random()*12)]
-  let randomType = cardType[Math.floor(Math.random()*4)]
+  document.getElementById("numero").innerHTML = cardNumbers[Math.floor(Math.random() *12)];
+  document.getElementById("pinta-top").innerHTML = randomType.type;
+  document.getElementById("pinta-bottom").innerHTML = randomType.type;
 
-
-  document.getElementById("numero").innerHTML = cardNumbers[Math.floor(Math.random()*12)];
-  document.getElementById("pinta").innerHTML = randomType.type;
-
-
-  document.getElementById("pinta").style.color = randomType.color;
   document.getElementById("numero").style.color = randomType.color;
-
-
+  document.getElementById("pinta-top").style.color = randomType.color;
+  document.getElementById("pinta-bottom").style.color = randomType.color;
 };
